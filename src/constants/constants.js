17f6f776cc0eb3819937532925f9791ce8e4a001
ashikapanda <82750291+ashikapanda@users.noreply.github.com>
2024-03-07 19:00:11 +0530

    Add files via upload

---
+++ src/constants/constants.js
@@ -0,0 +1,147 @@
+export const projects = [
+  {
+    title: "Home Space",
+    description:
+      "Home Space is an organization of highly creative and professionals in the field of architecture, interior design, and project management. ",
+    image: "homeSpace.png",
+    // image: "/images/jp01.jpg",
+    tags: ["HTML5", "CSS5", "Javascript", "React.js", "Redux"],
+
+    source: "https://wondrous-taiyaki-06d0d0.netlify.app/",
+    githubLink: "https://github.com/ashikapanda/HomeSpace",
+    visit: null,
+    id: 22,
+  },
+  {
+    title: "TaskSimple- Todo App",
+    description:
+      "TaskSimple is a simple Todo app which is developed using Nodejs for learning purpose",
+    image: "todo_app.jpg",
+    // image: "/images/jp01.jpg",
+    tags: ["Javascript", "React.js", "Redux", "Node.js", "Express.js"],
+    githubLink: "https://github.com/ashikapanda/TodoList_NodeExpress",
+    source: "",
+    visit: null,
+    id: 22,
+  },
+  {
+    title: "Simon Game",
+    description: "This is a simple Simon developed using Javascript & HTML",
+    image: "simon-says.png",
+    // image: "/images/project 001.png",
+    tags: ["HTML5", "CSS5", "Javascript"],
+    githubLink: "https://github.com/ashikapanda/SimonGame",
+    source: "",
+    visit: null,
+    id: 35,
+  },
+  {
+    title: "FoodVilla",
+    description:
+      "FoodVilla is a food ordering application developed taking Swiggy as reference for React learning purpose",
+    image: "foodVilla.jpg",
+    // image: "/images/jp02.png",
+    tags: ["HTML5", "CSS5", "Javascript", "React.js", "Redux", "bundler"],
+    githubLink: "https://github.com/ashikapanda/FoodVilla",
+    source: "",
+    visit: null,
+    id: 33,
+  },
+  {
+    title: "Youtube- clone",
+    description:
+      "This is a youtube clone which features like video playing, live chat, nested comments and more developed using React.js",
+    image: "uTube.jpg",
+
+    tags: ["HTML5", "CSS5", "Javascript", "React.js", "Redux", "bundler"],
+    githubLink: "https://github.com/ashikapanda/Youtube_clone",
+    source: "",
+    visit: null,
+    id: 33,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2013, text: "Passed my Higher SC in Science stream with 86.4%" },
+  {
+    year: 2018,
+    text: "Completed my B.Tech in Information Technology with 86.7%",
+  },
+  {
+    year: 2018,
+    text: "Started my career as a software developer with Infosys",
+  },
+];
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    "⚡ Develop highly interactive and easy to use Front End / User Interfaces for your web and mobile applications",
+    "⚡ Expertise in React for web and React Native for Mobile Applications",
+    "⚡ Integration of third party services such as Firebase / AWS / Digital Ocean",
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "react-native",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "logos:aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+  ],
+};
+
+export const ResumeUrl = "";
+export const GithubUrl = "https://github.com/ashikapanda";
+export const LinkedInUrl = "https://www.linkedin.com/in/ashika-panda/";
+export const FacebookUrl = "";
+export const MobileNumber = "+91 7978646832";
+export const BusinessEmail = "";
+export const PersonalEmail = "ashikapanda995@gmail.com";
