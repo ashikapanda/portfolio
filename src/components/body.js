6c1914dbbacdee051bed833deed6305d9afc2b3e
ashikapanda <82750291+ashikapanda@users.noreply.github.com>
2024-03-07 19:06:37 +0530

    Create body.js

---
+++ src/components/body.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Hero from "../components/Hero/Hero";
+// import Projects from "../components/Projects/Projects";
+// import Technologies from "../components/Technologies/Technologies";
+// import Timeline from "../components/TimeLine/TimeLine";
+// import { Outlet } from "react-router-dom";
+
+function Body() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Body;
